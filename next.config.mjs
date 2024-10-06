9f34be5b722422ab0d28ee2f0b660c452db11e81
Arul Karim <lucknoexcuse15@gmail.com>
2024-10-06 11:22:58 +0700

    fixing

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.externals = [...config.externals, 'prisma', '@prisma/client']
+    }
+    return config
+  },
 };
 
 export default nextConfig;
